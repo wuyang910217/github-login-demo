a422c41b582a08b69d6aae57bd66d3448ccbb0c2
wuyang910217 <wuyang910217@gmail.com>
2016-05-02 12:13:07 +0800

    判断用户是否是用github账号登录

--- server/github.js
+++ server/github.js
@@ -9,41 +9,45 @@ ServiceConfiguration.configurations.insert({
     secret: '2d7611b135a2d2298059f1ad07f6e72d211db1bf'
 });
 
-Accounts.onCreateUser(function (options, user) {
-  var accessToken = user.services.github.accessToken,
-      result,
-      profile;
+Accounts.onCreateUser(function(options, user) {
+    if (!user.services.github) {
+        return user;
+    } else {
+        var accessToken = user.services.github.accessToken,
+            result,
+            profile;
 
-  result = Meteor.http.get("https://api.github.com/user", {
-    headers: {"User-Agent": "Meteor/1.0"},
-    params: {
-      access_token: accessToken
+        result = Meteor.http.get("https://api.github.com/user", {
+            headers: { "User-Agent": "Meteor/1.0" },
+            params: {
+                access_token: accessToken
+            }
+        });
+
+        if (result.error)
+            throw result.error;
+
+        // profile = _.pick(result.data,
+        //   "name",  
+        //   "login",
+        //   "avatar_url",
+        //   "url",
+        //   "company",
+        //   "blog",
+        //   "location",
+        //   "email",
+        //   "bio",
+        //   "html_url");
+
+        profile = {
+            username: result.data.login,
+            name: result.data.name,
+            email: result.data.email,
+            url: result.data.html_url,
+            avatar_url: result.data.avatar_url,
+        };
+        user.profile = profile;
+        console.log(user.profile);
+        return user;
     }
-  });
-
-  if (result.error)
-    throw result.error;
-
-  // profile = _.pick(result.data,
-  //   "name",  
-  //   "login",
-  //   "avatar_url",
-  //   "url",
-  //   "company",
-  //   "blog",
-  //   "location",
-  //   "email",
-  //   "bio",
-  //   "html_url");
-
-  profile = {
-    username : result.data.login,
-    name : result.data.name,
-    email : result.data.email,
-    url : result.data.html_url,
-    avatar_url : result.data.avatar_url,
-  };
-  user.profile = profile;
-console.log(user.profile);
-  return user;
 });
